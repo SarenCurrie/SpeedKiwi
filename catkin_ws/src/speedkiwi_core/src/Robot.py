8dbf1a772dd810132d71b044ec480d2aa1b51f16
Saren Currie <saren@sarencurrie.com>
2015-08-04 22:05:46 +1200

    Sigh

--- catkin_ws/src/speedkiwi_core/src/Robot.py
+++ catkin_ws/src/speedkiwi_core/src/Robot.py
@@ -1,48 +1,64 @@
 from geometry_msgs.msg import Twist, Pose
 from nav_msgs.msg import Odometry
 from std_msgs.msg import String
+from math import sin, cos
+from speedkiwi_core.msg import move
 import rospy
 
 class Robot(object):
     """docstring for Robot"""
-    def __init__(self, robot_id):
+    def __init__(self, robot_id, velocity, angular_velocity, x_offset, y_offset, theta_offset):
         super(Robot, self).__init__()
         self.robot_id = robot_id
+        self.velocity = velocity
+        self.x_offset = x_offset
+        self.y_offset = y_offset
+        self.theta_offset = theta_offset
         self.odometry = ""
-
+        self.is_moving = False
+        
         def odometry_handler(data):
             """docstring for fname"""
             self.odometry = data
-        rospy.Subscriber(self.robot_id + "/odom", Odometry, odometry_handler)
+
+        rospy.Subscriber("/" + self.robot_id + "/odom", Odometry, odometry_handler)
 
     def forward(self, distance):
         """docstring for handle_position"""
         if not self.odometry == "":
-            pub = rospy.Publisher('topic', String, queue_size=10)
-
-            if not self.is_blocked():
+            
+            if not self.is_blocked() and not self.is_moving:
+                publisher = rospy.Publisher("/" + self.robot_id + "/move", move, queue_size=10)
                 p = self.get_position()
-                r = self.get_rotation()
-#                 pub.publish(p.x)
-#                 pub.publish(p.y)
-#                 pub.publish(p.z)
-#                 pub.publish(r.x)
-#                 pub.publish(r.y)
-#                 pub.publish(r.z)
-                print(p.x)
-                print(p.y)
-                print(p.z)
-                print(r.x)
-                print(r.y)
-                print(r.z)
+                x = distance * sin(p['theta'])
+                y = distance * cos(p['theta'])
+                x_normal = x * self.velocity / distance
+                y_normal = y * self.velocity / distance
+
+                msg = move()
+                msg.x = x_normal
+                msg.y = y_normal
+
+                publisher.publish(msg)
+
+        else:
+            rate = rospy.Rate(10)
+            rate.sleep()
+            self.forward(distance)
+    # def rotate(self, angle):
+    #     """docstring for rotate"""
+    #     if not self.odometry == "":
+    #
 
     def get_position(self):
         """gets this robot's position relative to where it started"""
-        return self.odometry.pose.pose.position
-
-    def get_rotation(self):
-        """gets this robot's rotation relative to where it started"""
-        return self.odometry.pose.pose.orientation
+        position = self.odometry.pose.pose.position
+        rotation = self.odometry.pose.pose.orientation
+        return {
+            'x': position.x + self.x_offset,
+            'y': position.y + self.y_offset,
+            'theta': rotation.z + self.theta_offset
+        }
 
     def is_blocked(self):
         """is this robot able to move forward"""
--- catkin_ws/src/speedkiwi_core/src/test.py
+++ catkin_ws/src/speedkiwi_core/src/test.py
@@ -4,10 +4,11 @@ import Robot
 import rospy
 
 rospy.init_node('test')
-robot = Robot.Robot('robot_0')
+robot = Robot.Robot('robot_0', 0.5, 0.5, 0 , 0, 0)
+
+robot.forward(1)
 
 rate = rospy.Rate(10)
 
 while not rospy.is_shutdown():
-    robot.forward(1)
     rate.sleep()
