198185f57e94803842aa675492e09aee6eef1126
zoercai <rcai861@aucklanduni.ac.nz>
2015-08-06 23:57:59 +1200

    new animal class

---
+++ catkin_ws/src/speedkiwi_core/src/Animal.py
@@ -0,0 +1,16 @@
+from geometry_msgs.msg import Twist, Pose
+from nav_msgs.msg import Odometry
+from std_msgs.msg import String
+from math import sin, cos
+import random
+import rospy
+
+from Robot import Robot
+
+class Animal(Robot):
+	def __init__(self, name):
+		Robot.__init__(self,name, 1, 3, 1, 1, 0)
+
+	def forward(self):
+		self.set_velocity(random.random()*10)
+	
