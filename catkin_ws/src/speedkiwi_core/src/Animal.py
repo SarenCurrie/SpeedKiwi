165b0ea9f20e57401b3540713250418636b10170
Karen Xie <kxie094@aucklanduni.ac.nz>
2015-08-08 04:07:40 -0500

    Added class comment. Removed redundant method.

--- catkin_ws/src/speedkiwi_core/src/Animal.py
+++ catkin_ws/src/speedkiwi_core/src/Animal.py
@@ -8,12 +8,11 @@ import rospy
 from Robot import Robot
 
 class Animal(Robot):
+	"""Subclass of Robot which implements random movement"""
 	def __init__(self, name):
 		Robot.__init__(self,name, 100, 3, 1, 1, 0)
 
-	def forward(self):
-		self.set_velocity(random.random()*10)
-
 	def execute_callback(self):
-		self.set_velocity(random.random()*10)
+		self.set_velocity(random.random())
+		# TODO: Change direction randomly.
 	
