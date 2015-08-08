4c4ca798b8aaf8a9cbfd70e203f5da8b6b7ca04b
kxie094 <kxie094@aucklanduni.ac.nz>
2015-08-08 16:55:46 +1200

    Changed animal behaviour.

--- catkin_ws/src/speedkiwi_core/src/Animal.py
+++ catkin_ws/src/speedkiwi_core/src/Animal.py
@@ -2,7 +2,7 @@ from geometry_msgs.msg import Twist, Pose
 from nav_msgs.msg import Odometry
 from std_msgs.msg import String
 from math import sin, cos
-import random
+from random import randint
 import rospy
 
 from Robot import Robot
@@ -10,11 +10,12 @@ from Robot import Robot
 class Animal(Robot):
 	"""Subclass of robot which implements random movement."""
 	def __init__(self, name):
-		Robot.__init__(self,name, 100, 3, 1, 1, 0)
-
-	def forward(self):
-		self.set_velocity(random.random()*10)
+		Robot.__init__(self, name, 100, 3, 1, 1, 0)
 
 	def execute_callback(self):
-		self.set_velocity(random.random()*10)
+		self.forward()
+		rospy.loginfo('Before time sleep')
+		time.sleep(random.randint(3,6))
+		rospy.loginfo('After time sleep')
+		self.stop()
 	
--- catkin_ws/src/speedkiwi_core/src/test.py
+++ catkin_ws/src/speedkiwi_core/src/test.py
@@ -2,21 +2,24 @@
 
 from Robot import Robot
 from DifferentRobot import DifferentRobot
+from Animal import Animal
 from MoveAction import MoveAction
 import rospy
+
 
 rospy.init_node('test')
 robot = Robot('robot_0', 0.5, 0.5, 0, 0, 0)
 robot1 = DifferentRobot('robot_1', 2, 2, 0, 0, 0)
+animal = Animal('robot_2')
 
 robot.add_action(MoveAction(100))
 robot1.forward()
->>>>>>> master
 
 rate = rospy.Rate(10)
 
 while not rospy.is_shutdown():
     robot.execute()
     robot1.execute()
+    animal.execute()
 
     rate.sleep()
