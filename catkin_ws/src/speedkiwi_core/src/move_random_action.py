93f9f15e11760673c5dd9cf825c64513b7f13108
zoercai <rcai861@aucklanduni.ac.nz>
2015-08-12 11:34:59 +1200

    added animal collision detection

---
+++ catkin_ws/src/speedkiwi_core/src/move_random_action.py
@@ -0,0 +1,33 @@
+from action import Action
+import math
+import rospy
+import random
+
+
+class MoveRandomAction(Action):
+    """
+    Random movement action for Animal
+    """
+
+    def __init__(self, d):
+        self.duration = d
+        self.counter = 0
+
+    def during(self, robot):
+        """Behaviour: robot moves forward and rotates randomly, and rotates when it detects collision"""
+        self.counter += 1
+        randint = random.randint(1, 5)
+
+        if 1 <= randint <= 4 and not robot.is_blocked():
+            robot.forward()
+        else:
+            robot.start_rotate()
+
+    def is_finished(self, robot):
+        if self.counter > self.duration:
+            return True
+        else:
+            return False
+
+    def finish(self, robot):
+        robot.stop()
