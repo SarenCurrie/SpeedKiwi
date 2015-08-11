310e015c2b7ae8fbba8ba8f091a29b8477e42454
Saren Currie <saren@sarencurrie.com>
2015-08-11 16:09:43 +1200

    Picker finalised.

--- catkin_ws/src/speedkiwi_core/src/main.py
+++ catkin_ws/src/speedkiwi_core/src/main.py
@@ -11,7 +11,7 @@ from math import pi
 
 rospy.init_node('main')
 
-robot = Robot('robot_0', 3, 0.05, 0, 0, pi/2)
+robot = Robot('robot_0', 3, 0.5, 0, 0, pi/2)
 robot1 = DifferentRobot('robot_1', 2, 2, 0, 0, pi/2)
 animal = Animal('robot_2', 2, 2, 0, 0, pi/2)
 
@@ -19,6 +19,10 @@ robot.add_action(MoveAction(75))
 robot.add_action(RotateAction("rotate_to_east"))
 robot.add_action(MoveAction(3.5))
 robot.add_action(RotateAction("rotate_to_south"))
+robot.add_action(MoveAction(75))
+robot.add_action(RotateAction("rotate_to_west"))
+robot.add_action(MoveAction(3.5))
+robot.add_action(RotateAction("rotate_to_north"))
 robot.add_action(MoveAction(75))
 
 # robot1.add_action(NavigateAction(50, 50))
