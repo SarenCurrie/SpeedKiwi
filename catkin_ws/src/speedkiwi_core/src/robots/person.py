f8f83a0a1c2e4bbdf54326b7ccf43294df9bb3b8
Karen Xie <kxie094@aucklanduni.ac.nz>
2015-08-22 18:08:51 +1200

    Removed unneeded lines.

--- catkin_ws/src/speedkiwi_core/src/robots/person.py
+++ catkin_ws/src/speedkiwi_core/src/robots/person.py
@@ -36,39 +36,3 @@ class Person(Robot):
             rospy.loginfo("Goal " + str(x_target) + "," + str(y_target))
 
         self.counter += 1
-
-        # goal_x = random.randint(self.min_x, self.max_x)
-        # goal_y = random.randint(self.min_x, self.max_y)
-
-        
-        
-        
-        # if not self.rotation_executing:
-        #     # # Blocking logic
-        #     # if self.is_blocked():
-        #     #     self.stop()
-        #     #     return
-        #     # self.forward()
-
-        #     # position = self.get_position()
-        #     # rospy.loginfo(str(position['y']))
-
-        #     if self.is_blocked() and self.direction is "east": 
-        #         self.rotate_to_west()
-        #         self.direction = "west"
-        #     elif self.is_blocked() and self.direction is "west":
-        #         self.rotate_to_east()
-        #         self.direction = "east"
-        #     # elif (position['y'] > 25) and self.direction is "east":
-        #     #     self.rotate_to_west()
-        #     #     self.direction = "west"
-        #     # elif (position['y'] < 5) and self.direction is "west":
-        #     #     self.rotate_to_east()
-        #     #     self.direction = "east"
-        #     self.forward()
-        
-        # else: 
-        #    if self.direction == "east":
-        #        self.rotate_to_east()
-        #    else:
-        #        self.rotate_to_west()
