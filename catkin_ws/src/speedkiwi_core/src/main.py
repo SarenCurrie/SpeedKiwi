8fc8aa4d0d7d0e51dbb3aec57da831226e651a5b
Chester <chester.booker.nz@gmail.com>
2015-08-23 16:08:50 +1200

    Changes to robot list, expected conflicts

--- catkin_ws/src/speedkiwi_core/src/main.py
+++ catkin_ws/src/speedkiwi_core/src/main.py
@@ -7,18 +7,29 @@ from math import pi
 
 rospy.init_node('main')
 
-robot = Robot('robot_0', 3, 0.5, -8.5, -37, pi/2)
-robot1 = DifferentRobot('robot_1', 2, 0.5, 0, 0, pi/2)
-animal = Animal('robot_2', 2, 2, 0, 0, pi/2)
-person = Person('robot_3', 2, 0.5, -17, 37, 0)
-person2 = EducatedPerson('robot_4', 2, 0.5, -17, 37, 0)
-binbot = Bin('robot_6',3, 0.5, -8.5, -37, pi/2)
-picker2 = PickerRobot('robot_9', 3, 0.5, 0, -45, 0)
-picker3 = PickerRobot('robot_10', 3, 0.5, 5, -40, 0)
+animal = Animal('robot_0', 2, 2, 19, -45, 0)
+
+person1 = Person('robot_1', 2, 0.5, 0, 40, 0)
+person2 = EducatedPerson('robot_2', 2, 0.5, 27, -48, 0)
+
+# Tractor ROBOT HERE: tractor = TractorRobot()
+
+binbot1 = Bin('robot_4',3, 0.5, -8.75, -38, 0)
+binbot2 = Bin('robot_5',3, 0.5, -8.75, -38, 0)
+binbot3 = Bin('robot_6',3, 0.5, -8.75, -38, 0)
+binbot4 = Bin('robot_7',3, 0.5, -8.75, -38, 0)
+
+picker1 = PickerRobot('robot_8', 3, 0.5, -8.75, -41, 0)
+picker2 = PickerRobot('robot_9', 3, 0.5, 1, -41, 0)
+picker3 = PickerRobot('robot_10', 3, 0.5, 5, -43, 0)
+
+robot = Robot('robot_11', 3, 0.5, -8.5, -37, 0) #Will be carrier #1
+robot1 = DifferentRobot('robot_12', 2, 0.5, 0, 0, 0) #Will be carrier #2
+
 
 # Testing bin mimicking:
-binbot.latch(robot)
-robot.add_slave(binbot)
+binbot1.latch(robot)
+robot.add_slave(binbot1)
 
 # # robot.add_action(RotateAction("rotate_to_north"))
 robot.add_action(MoveAction(1))
@@ -41,13 +52,19 @@ animal.add_action(NavigateAction(0, 40))
 rate = rospy.Rate(10)
 
 while not rospy.is_shutdown():
+    
     robot.execute()
     robot1.execute()
     animal.execute()
-    person.execute()
+    person1.execute()
     person2.execute()
-    binbot.execute()
+    binbot1.execute()
+    binbot2.execute()
+    binbot3.execute()
+    binbot4.execute()
+    picker1.execute()
     picker2.execute()
     picker3.execute()
+
 
     rate.sleep()
