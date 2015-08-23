ec37c08f9c20c029b03b44ead183d4f83aa57dcc
Karen Xie <kxie094@aucklanduni.ac.nz>
2015-08-23 14:00:08 +1200

    Subscriber receives message.

--- catkin_ws/src/speedkiwi_core/src/robots/educated_person.py
+++ catkin_ws/src/speedkiwi_core/src/robots/educated_person.py
@@ -1,12 +1,17 @@
 import rospy
 from std_msgs.msg import String
+from robots import Robot
 
-class EducatedPerson(object):
+class EducatedPerson(Robot):
 
 	def __init__(self, robot_id, top_speed, angular_top_speed, x_offset, y_offset, theta_offset):
 		Robot.__init__(self, robot_id, top_speed, angular_top_speed, x_offset, y_offset, theta_offset)
 		self.type = type(self).__name__
-		rospy.Subscriber("move_command", String, cmd_handler)
+		self.command = None
+		rospy.Subscriber("move_command", String, self.cmd_handler)
 
-	def cmd_handler(data):
-		rospy.loginfo("Move: ", data.data)
+	def cmd_handler(self, data):
+		rospy.loginfo("Move: %s", str(data.data))
+		
+
+
